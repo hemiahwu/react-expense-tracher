1b0e0e8b213e0a1730d8f4ff171a1a668b0a9fdc
ms-online <27732357@qq.com>
2022-11-29 15:48:13 +0800

    lesson-04

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import '../resources/default-layout.css';
 const DefaultLayout = (props) => {
   return (
     <div className='layout'>
       {/* header部分 */}
-      <div className='header'>
+      <div className='header d-flex justify-content-between align-items-center'>
         <div>
           <h1 className='logo'>我的记账本</h1>
+        </div>
+        <div>
+          <h1 className='username'>用户姓名</h1>
         </div>
       </div>
 
