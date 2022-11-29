79310ec6fee31e421cf9459b14b44f55674632c3
ms-online <27732357@qq.com>
2022-11-29 14:47:00 +0800

    lesson-02

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import { Button } from 'antd';
 function App() {
   return (
     <div className='App'>
       <h1>我的记账本</h1>
+      <Button type='primary'>Primary Button</Button>
     </div>
   );
 }
