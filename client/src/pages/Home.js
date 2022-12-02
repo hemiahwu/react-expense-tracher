cbd9b29eb69234cf7be5ecd845e16e0ecaea4c8a
ms-online <27732357@qq.com>
2022-12-02 11:32:44 +0800

    lesson-13

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import { Modal } from 'antd';
+import React, { useState } from 'react';
 import DefaultLayout from '../components/DefaultLayout';
+import '../resources/transaction.css';
 
 const Home = () => {
+  const [showAddEditTransactionModal, setShowAddEditTransactionModal] =
+    useState(false);
   return (
     <DefaultLayout>
-      <h1>这是主页</h1>
+      {/* 上方:过滤及类型切换，添加交易按钮 */}
+      <div className='filter d-flex justify-content-between align-items-center'>
+        <div></div>
+        <div>
+          <button
+            className='primary'
+            onClick={() => setShowAddEditTransactionModal(true)}
+          >
+            添加流水
+          </button>
+        </div>
+      </div>
+
+      {/* 下方:显示交易流水 */}
+      <div></div>
+
+      {/* 添加流水的modal弹出框 */}
+      <Modal
+        title='添加流水'
+        open={showAddEditTransactionModal}
+        onCancel={() => setShowAddEditTransactionModal(false)}
+      ></Modal>
     </DefaultLayout>
   );
 };
