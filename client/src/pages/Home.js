23bfe4922e6d2e15acd7c948263cd53d5eea0cc2
ms-online <27732357@qq.com>
2022-12-02 15:12:18 +0800

    lesson-14

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Modal } from 'antd';
+import { Form, Input, Modal, Select } from 'antd';
 import React, { useState } from 'react';
 import DefaultLayout from '../components/DefaultLayout';
 import '../resources/transaction.css';
@@ -6,6 +6,10 @@ import '../resources/transaction.css';
 const Home = () => {
   const [showAddEditTransactionModal, setShowAddEditTransactionModal] =
     useState(false);
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
   return (
     <DefaultLayout>
       {/* 上方:过滤及类型切换，添加交易按钮 */}
@@ -28,8 +32,93 @@ const Home = () => {
       <Modal
         title='添加流水'
         open={showAddEditTransactionModal}
+        footer={false}
         onCancel={() => setShowAddEditTransactionModal(false)}
-      ></Modal>
+      >
+        <Form
+          layout='vertical'
+          className='transaction-form'
+          onFinish={onFinish}
+        >
+          <Form.Item label='金额' name='amount'>
+            <Input type='text' />
+          </Form.Item>
+          <Form.Item label='类型' name='type'>
+            <Select
+              defaultValue='收入'
+              options={[
+                {
+                  value: 'income',
+                  label: '收入',
+                },
+                {
+                  value: 'expense',
+                  label: '支出',
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item label='分类' name='category'>
+            <Select
+              defaultValue='工资'
+              options={[
+                {
+                  value: 'salary',
+                  label: '工资',
+                },
+                {
+                  value: 'freelance',
+                  label: '兼职',
+                },
+                {
+                  value: 'food',
+                  label: '饮食',
+                },
+                {
+                  value: 'entertainment',
+                  label: '娱乐',
+                },
+                {
+                  value: 'investment',
+                  label: '投资',
+                },
+                {
+                  value: 'travel',
+                  label: '旅行',
+                },
+
+                {
+                  value: 'education',
+                  label: '教育',
+                },
+                {
+                  value: 'medical',
+                  label: '医疗',
+                },
+                {
+                  value: 'tax',
+                  label: '交通',
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item label='时间' name='date'>
+            <Input type='date' />
+          </Form.Item>
+          <Form.Item label='关联' name='reference'>
+            <Input type='text' />
+          </Form.Item>
+          <Form.Item label='描述' name='description'>
+            <Input type='text' />
+          </Form.Item>
+
+          <div className='d-flex justify-content-end'>
+            <button className='primary' type='submit'>
+              保存
+            </button>
+          </div>
+        </Form>
+      </Modal>
     </DefaultLayout>
   );
 };
