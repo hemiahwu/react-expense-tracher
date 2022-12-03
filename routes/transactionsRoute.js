3102a5228cb80228d1bb06d8e12cd749df61f1ad
ms-online <27732357@qq.com>
2022-12-03 17:09:10 +0800

    lesson-18

--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Transaction = require('../models/Transaction');
+const moment = require('moment');
 const router = express.Router();
 
 //添加流水API
@@ -16,7 +17,14 @@ router.post('/add-transaction', async (req, res) => {
 //获取所有交易流水API
 router.post('/get-all-transactions', async (req, res) => {
   try {
-    const transactions = await Transaction.find({ userid: req.body.userid });
+    const transactions = await Transaction.find({
+      date: {
+        //$gt: moment('2022-11-01).toDate(),
+        //$lt: moment('2022-11-01').toDate(),
+        // $gt: moment().subtract(365, 'd').toDate(),
+      },
+      userid: req.body.userid,
+    });
     res.send(transactions);
   } catch (error) {
     res.status(500).json(error);
